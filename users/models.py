856eda95d5505e44051655010efdf84e06f9ad53
Anastasiya Burykh <burykh_2000@mail.ru>
2023-07-15 23:28:48 +0300

    add users

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from main.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
